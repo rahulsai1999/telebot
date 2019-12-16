3c79a42c9adda47b08945ffa0770acc3bc7b5a88
rahulsai1999 <noneuser2183@gmail.com>
2019-12-16 23:20:19 +0530

    file upload test

--- app.py
+++ app.py
@@ -13,28 +13,31 @@ app = Flask(__name__)
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
+    file_id = update.message.file.id
+    x = bot.get_file(file_id=file_id)
     chat_id = update.message.chat.id
     msg_id = update.message.message_id
-
-    text = update.message.text.encode('utf-8').decode()
-    print("got text message :", text)
-    if text == "/start":
-        bot_welcome = """Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to 
-        generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with 
-        an avatar for your name. """
-        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
-    else:
-        try:
-            # clear the message we got from any non alphabets
-            text = re.sub(r"\W", "_", text)
-            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-            # note that you can send photos by url and telegram will fetch it for you
-            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-        except Exception:
-            bot.sendMessage(chat_id=chat_id,
-                            text="There was a problem in the name you used, please enter different name",
-                            reply_to_message_id=msg_id)
+    bot.send_photo(chat_id=chat_id, photo=x, reply_to_message_id=msg_id)
+    
+    # text = update.message.text.encode('utf-8').decode()
+    # print("got text message :", text)
+    # if text == "/start":
+    #     bot_welcome = """Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to
+    #     generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with
+    #     an avatar for your name. """
+    #     bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+    #
+    # else:
+    #     try:
+    #         # clear the message we got from any non alphabets
+    #         text = re.sub(r"\W", "_", text)
+    #         url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+    #         # note that you can send photos by url and telegram will fetch it for you
+    #         bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+    #     except Exception:
+    #         bot.sendMessage(chat_id=chat_id,
+    #                         text="There was a problem in the name you used, please enter different name",
+    #                         reply_to_message_id=msg_id)
 
     return 'ok'
 
