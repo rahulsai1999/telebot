c31f375d736c91b3df1aa1bd3cdab88087f25fd0
rahulsai1999 <noneuser2183@gmail.com>
2020-02-23 18:30:47 +0530

    changed itervalues to values

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def sendMarketInfo(bot,chat_id,msg_id,crop):
     
     records=r["records"]
     for i in records:
-        if crop in i.itervalues():
+        if crop in i.values():
             text_here="State: "+i["state"]+" Market: "+i["district"]+ "Price: "+i["modal_price"];
             bot.sendMessage(chat_id=chat_id,text=text_here,reply_to_message_id=msg_id)
 
